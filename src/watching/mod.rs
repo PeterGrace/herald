441550b46cac35d4155e550501511e1a4aeb1ce1
Peter Grace <pete.grace@gmail.com>
2020-08-07 08:15:31 -0400

    initial stab atcode

---
+++ src/watching/mod.rs
@@ -0,0 +1,22 @@
+use k8s_openapi::api::apps::v1::{
+    Deployment
+};
+use k8s_openapi::api::core::v1::{
+    Secret,
+    ConfigMap,
+    Service
+};
+use futures::future::SelectAll;
+use futures::stream::StreamFuture;
+use k8s_openapi::chrono::format::Item;
+
+pub(crate) enum WatchTypes {
+    ConfigMap(ConfigMap),
+    Deployment(Deployment),
+    Secret(Secret),
+    Service(Service)
+}
+
+fn setup_combined_watch_stream() -> SelectAll<Item> {
+
+}

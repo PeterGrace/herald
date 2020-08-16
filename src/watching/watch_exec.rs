dccbad0e1d4db2fa4624b1bb64adc33c543776b7
Peter Grace <pete.grace@gmail.com>
2020-08-16 18:28:51 -0400

    implement thread spawning and thread despawning based on input of Watchers

--- src/watching/watch_exec.rs
+++ src/watching/watch_exec.rs
@@ -1,4 +1,4 @@
-use futures::{stream, StreamExt, TryStreamExt};
+use futures::{StreamExt, TryStreamExt};
 use thiserror::Error;
 //use strum_macros::EnumString;
 //use std::str::FromStr;
@@ -8,15 +8,16 @@ use kube::{
 };
 //use k8s_openapi::apimachinery::pkg::apis::meta::v1::LabelSelector;
 pub use crate::watching::watch_types::WatchTypes;
-use kube_runtime::utils::try_flatten_applied;
 use kube_runtime::watcher;
 use prometheus::HistogramVec;
 use crate::models::watcher_item_spec::WatcherItemSpec;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, HashMap};
 use k8s_openapi::api::core::v1::Node;
 use k8s_openapi::api::apps::v1::Deployment;
 use kube_runtime::watcher::{Event};
 use kube::api::Meta;
+use tokio::sync::mpsc::{channel, Sender};
+use tokio::select;
 
 #[derive(Error, Debug)]
 pub enum WatchError {
@@ -39,7 +40,11 @@ lazy_static! {
 pub fn btree_to_string(input: BTreeMap<String, String>) -> String {
     let mut result = String::from("");
     for key in input.keys() {
-        result = format!("{}, {}={}", result, key, input.get(key).unwrap());
+        if result.len() == 0 {
+            result = format!("{}={}", key, input.get(key).unwrap());
+        } else {
+            result = format!("{}, {}={}", result, key, input.get(key).unwrap());
+        }
     }
     result
 }
@@ -52,18 +57,22 @@ pub async fn create_specific_watcher(input_obj: WatcherItemSpec) -> anyhow::Resu
         let lp = ListParams::default()
             .allow_bookmarks()
             .labels(label_str.as_str());
-
         let kind = input_obj.target_kind.unwrap();
+        info!("Creating watcher for kind {} with labelstr {}", kind, label_str);
         match kind.to_lowercase().as_str() {
             "deployment" => {
                 let watched: Api<Deployment> = Api::all(client);
                 let mut w = watcher(watched, lp).boxed();
                 while let Some(status) = w.try_next().await? {
                     match status {
-                        Event::Applied(s) => info!("Added object: {}", Meta::name(&s)),
-                        Event::Deleted(s) => info!("Modified object: {}", Meta::name(&s)),
-                        //Event::Restarted(s) => info!("Deleted object: {}", Meta::name(&s)),
-                        _ => info!("Error: {}", WatchError::Unknown(String::from("got a watch event we don't understand")))
+                        Event::Applied(s) => info!("Applied object: {}", Meta::name(&s)),
+                        Event::Deleted(s) => info!("Deleted object: {}", Meta::name(&s)),
+                        Event::Restarted(s) => {
+                            for deployment in s.iter() {
+                                info!("Restarted object: {}", Meta::name(deployment));
+                            }
+                        },
+                        //_ => info!("Error: {}", WatchError::Unknown(String::from("got a watch event we don't understand")))
                     }
                 }
             },
@@ -85,38 +94,69 @@ pub async fn create_specific_watcher(input_obj: WatcherItemSpec) -> anyhow::Resu
 }
 
 pub async fn create_and_start_watchers() -> anyhow::Result<()> {
+    let mut watch_registry: HashMap<String, Sender<()>> = HashMap::new();
     let client = Client::try_default().await?;
     let lp = ListParams::default().allow_bookmarks();
 
-    let mut stream_vec = Vec::new();
     //Watcher(Watcher),
-    stream_vec.push(
-        try_flatten_applied(watcher(Api::all(client.clone()), lp.clone()))
-            .map_ok(|watched| WatchTypes::Watcher(watched))
-            .map_err(|e| WatchError::UnknownThing(String::from("Watcher"), e.to_string()))
-            .boxed(),
-    );
-
-    let mut combined_stream = stream::select_all(stream_vec);
-    loop {
-        let result = combined_stream.try_next().await;
-        match result {
-            Ok(o) => match o {
-                Some(WatchTypes::Watcher(w)) => {
-                    for item in w.spec.watchers.unwrap() {
-                        tokio::spawn(async move {
-                            create_specific_watcher(item).await;
-                        });
-                    }
-                },
-                Some(_) => (),
-                None => info!("Error on reading"),
+    let watched: Api<crate::models::watcher_spec::Watcher> = Api::all(client);
+    let mut w = watcher(watched, lp).boxed();
+    while let Some(watcher_status) = w.try_next().await? {
+        match watcher_status {
+            Event::Applied(s) => {
+                info!("Processing apply on Watcher watch");
+                for item in s.spec.watchers.unwrap() {
+                    let label_str = btree_to_string(item.clone().selector.unwrap().match_labels.unwrap());
+                    let id = format!("{}+{}",item.clone().target_kind.unwrap(),label_str);
+                    info!("Inserting channel into watch registry for key {}", id);
+                    let (tx, mut rx) = channel(1);
+                    tokio::spawn(async move {
+                        select!{
+                         _ = create_specific_watcher(item) => info!("watching thread exited...?"),
+                         _ = rx.next() => info!("Received word we should exit")
+                        }
+                    });
+                    watch_registry.insert(id,  tx);
+                }
             },
-            Err(e) => {
-                info!("Error: {}", e);
-            }
+            Event::Deleted(s) => {
+                info!("Processing delete on Watcher watch");
+                for item in s.spec.watchers.unwrap() {
+                    let label_str = btree_to_string(item.clone().selector.unwrap().match_labels.unwrap());
+                    let id = format!("{}+{}",item.clone().target_kind.unwrap(),label_str);
+                    if let Some(watch_channel) = watch_registry.get_mut(&id) {
+                        watch_channel.send(());
+                    }
+                }
+            },
+            Event::Restarted(s) => {
+                info!("Processing restart on Watcher watch");
+                for object in s.iter() {
+                    let watchVec = object.spec.watchers.clone();
+                    for item in watchVec.unwrap() {
+                        let label_str = btree_to_string(item.clone().selector.unwrap().match_labels.unwrap());
+                        let id = format!("{}+{}",item.clone().target_kind.unwrap(),label_str);
+                        if let Some(watch_channel) = watch_registry.get_mut(&id) {
+                            info!("killing channel for key {}", id);
+                            watch_channel.send(());
+                        }
+                        info!("Inserting channel into watch registry for key {}", id);
+                        let (tx, mut rx) = channel(1);
+                        tokio::spawn(async move {
+                            select!{
+                         _ = create_specific_watcher(item) => info!("watching thread exited...?"),
+                         _ = rx.next() => info!("Received word we should exit")
+                        }
+                        });
+                        watch_registry.insert(id,  tx);
+                    }
+
+                }
+            },
+            //_ => info!("Error: {}", WatchError::Unknown(String::from("got a watch event we don't understand")))
         }
     }
+    Ok(())
 }
 
 // fn process_watcher(w: crate::models::watcher_spec::Watcher) -> () {

e879a3f438c53a6510a9e94d313ea20822ee8cdf
Peter Grace <pete.grace@gmail.com>
2020-08-30 01:49:37 +0000

    test a get send

--- src/tests/test_send_hook.rs
+++ src/tests/test_send_hook.rs
@@ -1,11 +1,8 @@
 extern crate httpmock;
 
-use httpmock::Method::{POST};
-use httpmock::{
-    Mock,
-    MockServer
-};
 use crate::watching::send_hook::send_hook;
+use httpmock::Method::POST;
+use httpmock::{Mock, MockServer};
 
 #[tokio::test]
 async fn test_send_hook() {
@@ -16,7 +13,36 @@ async fn test_send_hook() {
         .return_status(200)
         .create_on(&mock_server);
 
-    let response = send_hook(mock_server.url("/mockpost").to_string(), String::from("POST"), String::from("{}")).await.unwrap();
+    let response = send_hook(
+        mock_server.url("/mockpost").to_string(),
+        String::from("POST"),
+        String::from("{}"),
+    )
+    .await
+    .unwrap();
     assert_eq!(response.status(), 200);
+    assert_eq!(search_mock.times_called(), 1);
+}
+#[tokio::test]
+
+async fn test_send_get_hook() {
+    let mock_server = MockServer::start();
+
+    let search_mock = Mock::new()
+        .expect_method(GET)
+        .expect_path("/mockpost")
+        .return_status(200)
+        .create_on(&mock_server);
+
+    let response = send_hook(
+        mock_server.url("/mockpost").to_string(),
+        String::from("GET"),
+        String::from("{}"),
+    )
+    .await
+    .unwrap();
+
+    assert_eq!(response.status(), 200);
+
     assert_eq!(search_mock.times_called(), 1);
 }

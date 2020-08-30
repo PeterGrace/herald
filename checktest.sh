ec10b779a1a3b1cd580ca0d49564b6a95ee67ec3
Peter Grace <pete.grace@gmail.com>
2020-08-30 00:57:56 +0000

    Xml not xml

--- checktest.sh
+++ checktest.sh
@@ -3,4 +3,4 @@
 CARGO_INCREMENTAL=0
 RUSTFLAGS='-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
 RUSTDOCFLAGS='-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Cpanic=abort -Zpanic_abort_tests'
-cargo tarpaulin -o xml --ciserver jenkins
+cargo tarpaulin -o Xml --ciserver jenkins
